db74545c540358a3d066b20db5fa5f1bbde9be1f
Charlie Zhu <goodgoodwish@gmail.com>
2020-09-01 12:01:26 -0700

    part 4 django admin site

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Author, Genre, Book, BookInstance, Language
+
+class AuthorsBookInline(admin.TabularInline):
+    model = Book
+
+# admin.site.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [AuthorsBookInline]
+
+admin.site.register(Author, AuthorAdmin)
+
+admin.site.register(Genre)
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+# admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+# admin.site.register(BookInstance)
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+admin.site.register(Language)
+
